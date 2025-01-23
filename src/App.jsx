b32c8430162dab5ab62025e27e908b8a83698b75
CARLA LOPEZ <carlalolajeanne@gmail.com>
2025-01-23 13:23:48 +0100

    :lipstick: dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from "./page/home.jsx";
+import { ThemeProvider } from './Components/ThemeContext/themeContext.jsx';
 
 export default function App() {
 return(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home />}></Route>
-        </Routes>
-    </BrowserRouter>
+    <ThemeProvider>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+            </Routes>
+        </BrowserRouter>
+    </ThemeProvider>
 )}
--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -2,8 +2,12 @@ import { FaHouseUser } from "react-icons/fa";
 import { IoRocketSharp } from "react-icons/io5";
 import { FaGears } from "react-icons/fa6";
 import './navbar.styles.css'
+import { useTheme } from "../ThemeContext/themeContext";
+import { TbSunMoon } from "react-icons/tb";
 
 export default function Navbar(){
+
+    const { isDarkMode, toggleTheme } = useTheme();
     return(
         <>
             <header>
@@ -11,13 +15,20 @@ export default function Navbar(){
                     <p>By Carla</p>
                 </div>
                 <div className='link'>
-                    <FaHouseUser /><a href='#profile'>Profile</a>
+                    <FaHouseUser /><a href='#profileSection'>Profile</a>
                 </div>
                 <div className='link'>
                     <FaGears /><a href='#skillsSection'>Skills</a>
                 </div>
                 <div className='link'>
                     <IoRocketSharp /><a href='#projectSection'>Projects</a>
+                </div>
+                <div className="theme-toggle">
+                    <TbSunMoon />
+                    <label className="toggle-switch">
+                        <input type="checkbox" checked={isDarkMode} onChange={toggleTheme}/>
+                        <span className="slider"></span>
+                    </label>
                 </div>
             </header>
         </>
